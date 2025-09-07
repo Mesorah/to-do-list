d6d5ee76c0fc4ea9c7ba4975066aff756f31acef
Messorah <garabrielra@gmail.com>
2025-09-07 09:02:49 -0300

    http_method_names corrigida

--- tdl/views/api.py
+++ tdl/views/api.py
@@ -23,7 +23,7 @@ class ItemAPIViewSet(ModelViewSet):
     serializer_class = ItemSerializer
     pagination_class = ItemApiPagination
     permission_classes = [IsAuthenticatedOrReadOnly]
-    http_method_names = ['GET', 'OPTIONS', 'HEAD', 'PATCH', 'POST', 'DELETE']
+    http_method_names = ['get', 'options', 'head', 'patch', 'post', 'delete']
 
     def get_permissions(self):
         if self.request.method in ['PATCH', 'DELETE']:
